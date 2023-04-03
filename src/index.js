7606d3d403b56e8099f805baabb4cbb04e7f7187
Malik_Kashif <malikmuhammadkashif414@gmail.com>
2023-04-04 01:28:56 +0500

    level 3 exercise completed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import ReactApexChart from 'react-apexcharts';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export const tenHighestPopulation = [
+  { country: 'World', population: 7693165599 },
+  { country: 'China', population: 1377422166 },
+  { country: 'India', population: 1295210000 },
+  { country: 'United States of America', population: 323947000 },
+  { country: 'Indonesia', population: 258705000 },
+  { country: 'Brazil', population: 206135893 },
+  { country: 'Pakistan', population: 194125062 },
+  { country: 'Nigeria', population: 186988000 },
+  { country: 'Bangladesh', population: 161006790 },
+  { country: 'Russian Federation', population: 146599183 },
+  { country: 'Japan', population: 126960000 },
+]
+
+const App =() => {
+  const data1= {      
+    series: [{
+      data: [7693165599, 1377422166, 1295210000, 323947000, 258705000, 206135893, 194125062, 186988000, 161006790,146599183, 126960000]
+    }],
+    options: {
+      chart: {
+        type: 'bar',
+        height: 350
+      },
+      plotOptions: {
+        bar: {
+          borderRadius: 4,
+          horizontal: true,
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      xaxis: {
+        categories: ['World', 'China', 'India', 'United States of America', 'Indonesia', 'Brazil', 'Pakistan',
+          'Nigeria', 'Bangladesh', 'Russian Federation', 'Japan'
+        ],
+      }
+    }, 
+  };
+
+  return(
+    <>
+    <div className='app'>
+        <div className='app-wrapper'>
+          <h1>30 Days of React</h1>
+          <h3>World Population</h3>
+          <small>Top ten countires with Population</small>
+      </div>
+      <div style={{ marginLeft: '20%'}}>
+      <ReactApexChart options={data1.options} series={data1.series} type="bar" height={450} width={800} />
+      </div>
+    </div>
+
+    </>
+  )
+}
+
+const rootElement = ReactDOM.createRoot(document.getElementById('root'))
+// we render the App component using the ReactDOM package
+rootElement.render(<App />, rootElement)
